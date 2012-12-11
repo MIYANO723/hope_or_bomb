dfd3c8e9099ae37cfedd5bbc06d0f6455233a3eb
MIYANO SYUNICHI <cryforyouing@yahoo.co.jp>
2012-12-11 16:57:06 +0900

    Scroll
    背景のスクロール

--- hope_or_bomb.js
+++ hope_or_bomb.js
@@ -1,12 +1,37 @@
 ﻿enchant();
+  //背景クラス
+var Background = enchant.Class.create(enchant.Sprite, {
+    initialize: function(){
+	//ちょっと大きめの背景を用意する
+	enchant.Sprite.call(this,640,320);
+	this.x = 0;
+	this.y = 0;
+	this.image = game.assets['BG.jpg'];
+	this.addEventListener('enterframe', function(){
+	    //ひたすら背景をスクロール
+	    this.x--;
+	    //端まで来たら、背景を巻き戻す。この繰り返し
+	    if(this.x<=-320)this.x=0;
+	});
+	game.rootScene.addChild(this);
+    }
+});
 window.onload = function() {
+  
 	//ゲームオブジェクトの作成
-	var game = new Game(320,320) ;
-	game.preload('heri.png','hope.png','hopeswitch.png','bomb.png','bombswitch.png','effect0.png','BG.gif');
+	game = new Game(320,320) ;
+	game.preload('heri.png','hope.png','hopeswitch.png','bomb.png','bombswitch.png','effect0.png','BG.jpg');
 	game.score = 0;
-	game.fps = 16; 
+	game.fps = 24; 
 	var heri;
 	game.onload = function(){
+	  
+		background = new Background(); 
+    
+	  
+    enemies = [];
+		
+		
 		
 		var heri = new Sprite(95,117);
 		heri.image = game.assets['heri.png'];
@@ -15,33 +40,21 @@ window.onload = function() {
 		heri.anim  = [0,1,2];
 		heri.frame = 0;
 	  game.rootScene.addChild(heri);
-		//heri._element.style.zIndex = 20;
 		
 		var hopeswitch = new Sprite(61,61);
 		hopeswitch.image = game.assets['hopeswitch.png'];
 		hopeswitch.x     = 179;
-		hopeswitch.y     = 40;
+		hopeswitch.y     = 20;
 		hopeswitch.frame = 0;
 	  game.rootScene.addChild(hopeswitch);
-		//hopeswitch._element.style.zIndex = 20;
 		
 		var bombswitch = new Sprite(61,61);
 		bombswitch.image = game.assets["bombswitch.png"];
 		bombswitch.x     = 249;
-		bombswitch.y     = 40;
+		bombswitch.y     = 20;
 		bombswitch.frame = 0;
 	  game.rootScene.addChild(bombswitch);
-		//bombswitch._element.style.zIndex = 20;
 		
-		//背景画像の設定
-		var BG = new Sprite(320,320);
-		BG.image = game.assets['BG.gif'];
-		BG.x     = 0;
-    BG.y     = 0;
-    BG.frame = 0;
-    BG._element.style.zIndex = 30;
-    game.rootScene.addChild(BG);
-    
     };
   game.start();
   
