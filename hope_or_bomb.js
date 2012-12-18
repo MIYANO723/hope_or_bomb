58d0d816d842456cb1b5a1da2cc5fd92c5427336
MIYANO SYUNICHI <cryforyouing@yahoo.co.jp>
2012-12-18 15:38:21 +0900

    PhySprite
    物理エンジンPｈｙの導入

--- hope_or_bomb.js
+++ hope_or_bomb.js
@@ -1,5 +1,5 @@
 ﻿enchant();
-  //背景クラス
+//背景クラス
 var Background = enchant.Class.create(enchant.Sprite, {
     initialize: function(){
 	//ちょっと大きめの背景を用意する
@@ -20,18 +20,17 @@ window.onload = function() {
   
 	//ゲームオブジェクトの作成
 	game = new Game(320,320) ;
-	game.preload('heri.png','hope.png','hopeswitch.png','bomb.png','bombswitch.png','effect0.png','BG.jpg');
+	game.preload('Building.png','heri.png','hope.png','hopeswitch.png','bombswitch.png','effect0.png','BG.jpg','bomb.png');
 	game.score = 0;
 	game.fps = 24; 
 	var heri;
 	game.onload = function(){
+		//物理世界の創造
+		physicsWorld = new PhysicsWorld(0, 9.8);
 	  
 		background = new Background(); 
     
-	  
     enemies = [];
-		
-		
 		
 		var heri = new Sprite(95,117);
 		heri.image = game.assets['heri.png'];
@@ -49,12 +48,28 @@ window.onload = function() {
 	  game.rootScene.addChild(hopeswitch);
 		
 		var bombswitch = new Sprite(61,61);
-		bombswitch.image = game.assets["bombswitch.png"];
+		bombswitch.image = game.assets['bombswitch.png'];
 		bombswitch.x     = 249;
 		bombswitch.y     = 20;
 		bombswitch.frame = 0;
 	  game.rootScene.addChild(bombswitch);
-		
+	  
+	  //ボムスイッチがタッチされたとき
+	  bombswitch.ontouchstart = function(){
+	  	alert("残念ながら爆弾は出ない");
+	  };
+	  
+    var bomb = new PhyCircleSprite(8, DYNAMIC_SPRITE, 1.0, 0.5, 0.2,true);
+    bomb.image = game.assets['bomb.png'];
+    bomb.frame = 0;
+    bomb.position = { x: 160, y: 10 };
+    game.rootScene.addChild(bomb);
+    
+    game.rootScene.onentergrame = function(){
+    	
+	    world.step(game.fps)
+	    
+	    };
     };
   game.start();
   
